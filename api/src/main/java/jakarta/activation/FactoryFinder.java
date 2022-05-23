95378d08d61cf34be06c29180f12eb1b07e397ed
Jim Ma <ema@redhat.com>
2022-05-23 17:41:54 +0800

    Allow FactoryFinder to try to load class with jakarta api class's classloader

--- api/src/main/java/jakarta/activation/FactoryFinder.java
+++ api/src/main/java/jakarta/activation/FactoryFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -66,8 +66,11 @@ class FactoryFinder {
             if (result != null) {
                 return result;
             }
+            result = newInstance(className, defaultClassName, FactoryFinder.class.getClassLoader());
+            if (result != null) {
+                return result;
+            }
         }
-
         // standard services: java.util.ServiceLoader
         T factory = ServiceLoaderUtil.firstByServiceLoader(
                 factoryClass,
