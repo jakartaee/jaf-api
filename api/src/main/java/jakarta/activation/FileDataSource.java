a6ef80c37c4ab9f364b88f0020e2591b60ac9d90
Rob McDougall <rmcdougawww@rogers.com>
2023-04-05 07:55:30 -0400

    Restored previous code formatting.

--- api/src/main/java/jakarta/activation/FileDataSource.java
+++ api/src/main/java/jakarta/activation/FileDataSource.java
@@ -19,20 +19,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * The FileDataSource class implements a simple DataSource object that
- * encapsulates a file. It provides data typing services via a FileTypeMap
- * object.
- * <p>
+ * The FileDataSource class implements a simple DataSource object
+ * that encapsulates a file. It provides data typing services via
+ * a FileTypeMap object. <p>
  *
- * <b>FileDataSource Typing Semantics</b>
- * <p>
+ * <b>FileDataSource Typing Semantics</b><p>
  *
- * The FileDataSource class delegates data typing of files to an object
- * subclassed from the FileTypeMap class. The <code>setFileTypeMap</code> method
- * can be used to explicitly set the FileTypeMap for an instance of
- * FileDataSource. If no FileTypeMap is set, the FileDataSource will call the
- * FileTypeMap's getDefaultFileTypeMap method to get the System's default
- * FileTypeMap.
+ * The FileDataSource class delegates data typing of files
+ * to an object subclassed from the FileTypeMap class.
+ * The <code>setFileTypeMap</code> method can be used to explicitly
+ * set the FileTypeMap for an instance of FileDataSource. If no
+ * FileTypeMap is set, the FileDataSource will call the FileTypeMap's
+ * getDefaultFileTypeMap method to get the System's default FileTypeMap.
  *
  * <b>API Note:</b>
  * It is recommended to construct a {@code FileDataSource} using a {@code Path}
@@ -44,123 +42,124 @@ import java.nio.file.Paths;
  */
 public class FileDataSource implements DataSource {
 
-	// keep track of original 'ref' passed in, non-null
-	// one indicated which was passed in:
+    // keep track of original 'ref' passed in, non-null
+    // one indicated which was passed in:
 	private Path _path = null;
-	private FileTypeMap typeMap = null;
+    private FileTypeMap typeMap = null;
 
-	/**
-	 * Creates a FileDataSource from a File object. <i>Note: The file will not
-	 * actually be opened until a method is called that requires the file to be
-	 * opened.</i>
-	 *
-	 * <b>API Note:</b>
-	 * {@code FileDataSource(Path)} constructor should be preferred over this one.
-	 * 
-	 * @param file the file
-	 */
-	public FileDataSource(File file) {
-		_path = file.toPath(); // save the file as Path object...
-	}
+    /**
+     * Creates a FileDataSource from a File object. <i>Note:
+     * The file will not actually be opened until a method is
+     * called that requires the file to be opened.</i>
+     *
+     * <b>API Note:</b>
+     * {@code FileDataSource(Path)} constructor should be preferred over this one.
+     * 
+     * @param file the file
+     */
+    public FileDataSource(File file) {
+	_path = file.toPath();	// save the file Object...
+    }
 
-	/**
-	 * Creates a FileDataSource from a File object. <i>Note: The file will not
-	 * actually be opened until a method is called that requires the file to be
-	 * opened.</i>
-	 *
-	 * @param path the file
-	 */
-	public FileDataSource(Path path) {
-		_path = path;
-	}
+    /**
+     * Creates a FileDataSource from a Path object. <i>Note: The file will not
+     * actually be opened until a method is called that requires the file to be
+     * opened.</i>
+     *
+     * @param path the file
+     */
+    public FileDataSource(Path path) {
+    _path = path;
+    }
 
-	/**
-	 * Creates a FileDataSource from the specified path name. <i>Note: The file will
-	 * not actually be opened until a method is called that requires the file to be
-	 * opened.</i>
-	 *
-	 * @param name the system-dependent file name.
-	 */
-	public FileDataSource(String name) {
-		this(Paths.get(name)); // use the file constructor
-	}
+    /**
+     * Creates a FileDataSource from
+     * the specified path name. <i>Note:
+     * The file will not actually be opened until a method is
+     * called that requires the file to be opened.</i>
+     *
+     * @param name the system-dependent file name.
+     */
+    public FileDataSource(String name) {
+	this(Paths.get(name));	// use the file constructor
+    }
 
-	/**
-	 * This method will return an InputStream representing the the data and will
-	 * throw an IOException if it can not do so. This method will return a new
-	 * instance of InputStream with each invocation.
-	 *
-	 * @return an InputStream
-	 */
-	public InputStream getInputStream() throws IOException {
-		return Files.newInputStream(_path);
-	}
+    /**
+     * This method will return an InputStream representing the
+     * the data and will throw an IOException if it can
+     * not do so. This method will return a new
+     * instance of InputStream with each invocation.
+     *
+     * @return an InputStream
+     */
+    public InputStream getInputStream() throws IOException {
+	return Files.newInputStream(_path);
+    }
 
-	/**
-	 * This method will return an OutputStream representing the the data and will
-	 * throw an IOException if it can not do so. This method will return a new
-	 * instance of OutputStream with each invocation.
-	 *
-	 * @return an OutputStream
-	 */
-	public OutputStream getOutputStream() throws IOException {
-		return Files.newOutputStream(_path);
-	}
+    /**
+     * This method will return an OutputStream representing the
+     * the data and will throw an IOException if it can
+     * not do so. This method will return a new instance of
+     * OutputStream with each invocation.
+     *
+     * @return an OutputStream
+     */
+    public OutputStream getOutputStream() throws IOException {
+	return Files.newOutputStream(_path);
+    }
 
-	/**
-	 * This method returns the MIME type of the data in the form of a string. This
-	 * method uses the currently installed FileTypeMap. If there is no FileTypeMap
-	 * explictly set, the FileDataSource will call the
-	 * <code>getDefaultFileTypeMap</code> method on FileTypeMap to acquire a default
-	 * FileTypeMap. <i>Note: By default, the FileTypeMap used will be a
-	 * MimetypesFileTypeMap.</i>
-	 *
-	 * @return the MIME Type
-	 * @see jakarta.activation.FileTypeMap#getDefaultFileTypeMap
-	 */
-	public String getContentType() {
-		// check to see if the type map is null?
-		if (typeMap == null)
-			return FileTypeMap.getDefaultFileTypeMap().getContentType(_path);
-		else
-			return typeMap.getContentType(_path);
-	}
+    /**
+     * This method returns the MIME type of the data in the form of a
+     * string. This method uses the currently installed FileTypeMap. If
+     * there is no FileTypeMap explicitly set, the FileDataSource will
+     * call the <code>getDefaultFileTypeMap</code> method on
+     * FileTypeMap to acquire a default FileTypeMap. <i>Note: By
+     * default, the FileTypeMap used will be a MimetypesFileTypeMap.</i>
+     *
+     * @return the MIME Type
+     * @see jakarta.activation.FileTypeMap#getDefaultFileTypeMap
+     */
+    public String getContentType() {
+    // check to see if the type map is null?
+    if (typeMap == null)
+        return FileTypeMap.getDefaultFileTypeMap().getContentType(_path);
+	else
+        return typeMap.getContentType(_path);
+    }
 
-	/**
-	 * Return the <i>name</i> of this object. The FileDataSource will return the
-	 * file name of the object.
-	 *
-	 * @return the name of the object.
-	 * @see jakarta.activation.DataSource
-	 */
-	public String getName() {
-		return _path.getFileName().toString();
-	}
+    /**
+     * Return the <i>name</i> of this object. The FileDataSource
+     * will return the file name of the object.
+     *
+     * @return the name of the object.
+     * @see jakarta.activation.DataSource
+     */
+    public String getName() {
+	return _path.getFileName().toString();
+    }
 
-	/**
-	 * Return the File object that corresponds to this FileDataSource.
-	 * 
-	 * @return the File object for the file represented by this object.
-	 */
-	public File getFile() {
-		return _path.toFile();
-	}
+    /**
+     * Return the File object that corresponds to this FileDataSource.
+     * @return the File object for the file represented by this object.
+     */
+    public File getFile() {
+	return _path.toFile();
+    }
 
-	/**
-	 * Return the File object that corresponds to this FileDataSource.
-	 * 
-	 * @return the File object for the file represented by this object.
-	 */
-	public Path getPath() {
-		return _path;
-	}
+    /**
+     * Return the Path object that corresponds to this FileDataSource.
+     * @return the Path object for the file represented by this object.
+     */
+    public Path getPath() {
+	return _path;
+    }
 
-	/**
-	 * Set the FileTypeMap to use with this FileDataSource
-	 *
-	 * @param map The FileTypeMap for this object.
-	 */
-	public void setFileTypeMap(FileTypeMap map) {
-		typeMap = map;
-	}
+    /**
+     * Set the FileTypeMap to use with this FileDataSource
+     *
+     * @param map The FileTypeMap for this object.
+     */
+    public void setFileTypeMap(FileTypeMap map) {
+	typeMap = map;
+    }
 }
