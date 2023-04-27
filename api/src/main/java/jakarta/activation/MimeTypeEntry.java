ad18b5677a01dd87d680a9256c33fec0e42b5156
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-04-27 11:31:16 +0200

    add missing javadoc description for MimeTypeEntry getters
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- api/src/main/java/jakarta/activation/MimeTypeEntry.java
+++ api/src/main/java/jakarta/activation/MimeTypeEntry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -29,6 +29,8 @@ public class MimeTypeEntry {
     }
 
     /**
+     * Get MIME type string
+     *
      * @return the MIME type string
      */
     public String getMIMEType() {
@@ -36,6 +38,8 @@ public class MimeTypeEntry {
     }
 
     /**
+     * Get the file extension
+     *
      * @return the file extension
      */
     public String getFileExtension() {
